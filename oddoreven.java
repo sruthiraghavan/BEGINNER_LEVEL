6c222440160c3e740d956d156f16770ade50aeb3
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-04-18 13:23:05 +0530

    Update oddoreven.java

--- oddoreven.java
+++ oddoreven.java
@@ -1,1 +1,11 @@
-# BEGINNER_LEVEL
+class oddoreven
+{
+  public static void main(String args[])
+  {
+    int n=Integer.valueOf(args[0]);
+    if(n%2==0)
+      System.out.print("EVEN");
+    else
+      System.out.print("ODD");
+  }
+}
