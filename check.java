c5fdc372b8007fc9489b397fb7245c123ec859e8
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-04 18:12:40 -0700

    Create check.java

---
+++ check.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class check
+{
+    public  static void main(String args[])
+    {
+      int a=Integer.parseInt(args[0]);
+      if(a!=0)
+      {
+          if(a>0)
+          {
+              System.out.println("positive");
+          }
+          else
+          {
+              System.out.println("negative");
+          }
+      }
+      else
+    {
+        System.out.println("zero");
+    }
+    }
+}
