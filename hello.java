dbc9b09545fc65c76727ba19333d2d938ada80b8
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-04 17:57:59 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class leap
+{
+    public  static void main(String args[])
+    {
+      String s=args[0];
+      for(int i=0;i<5;i++)
+      {
+          System.out.println("hello");
+      }
+    }
+}
