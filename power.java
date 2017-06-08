62fdde40a6400a4b3264b1e25d56c97f2dbddecc
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-08 09:54:18 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,10 @@
+import java.math.*;
+class power
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+System.out.print(Math.pow(a,b));
+}
+}
