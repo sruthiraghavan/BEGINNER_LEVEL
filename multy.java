dccd3ffb799ddc79d8148f3e3dad750b3956cc15
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-12 03:52:22 -0700

    Create multy.java

---
+++ multy.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class multy
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+for(int i=0;i<=10;i++)
+{
+System.out.println(a+"*"+i+"="+a*i);
+}
+}
+}
+
