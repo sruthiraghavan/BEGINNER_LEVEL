a28ee24a0399c5f507802138a5dc0686c5c06d5e
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-12 03:39:47 -0700

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class compare
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.nextLine();
+String s2=sc.nextLine();
+if(s1.equals(s2))
+{
+System.out.print("the strings are equal");
+}
+else
+{
+System.out.print("not equal");
+}
+}
+}
