d89b0bd1d517eb7d2e65e22791517a1d0a9cd645
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-12 02:27:40 -0700

    Create greatest.java

---
+++ greatest.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class greatest
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+int c=s.nextInt();
+if(a>b)
+{
+System.out.println("a is the greatest");
+}
+else if(b>c)
+{
+System.out.println("b is the greatest");
+}
+else
+{
+System.out.println("c is the greatest");
+}
+}
+}
