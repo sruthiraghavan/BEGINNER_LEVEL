ab0a809cf2c48c6d010792974d6cde89854f7b3a
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-11 21:38:04 -0700

    Create evenin.java

---
+++ evenin.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class evenin
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+for(int i=a;i<=b;i++)
+{
+if(i%2==0)
+System.out.println(i);
+}
+}
+}
